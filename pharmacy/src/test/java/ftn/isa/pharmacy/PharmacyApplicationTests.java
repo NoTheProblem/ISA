76d454c4d78a3528edcba33312f760a15c74258e
NoTheProblem <lazarevic.branislav682@gmail.com>
2020-11-23 20:45:05 +0100

    init

---
+++ pharmacy/src/test/java/ftn/isa/pharmacy/PharmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package ftn.isa.pharmacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
