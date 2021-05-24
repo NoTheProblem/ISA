2b591ef31f4446642522774a1775a3a0e316c1f1
Aleksa Milenović <aleksa.milenovic@gmail.com>
2021-05-24 23:36:09 +0200

    Napravljen kostur na frontu za profile administratora sistema i dobavljaca, i povezane sve stranice

---
+++ pharmacy-front/src/app/register-medicine/register-medicine.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-medicine',
+  templateUrl: './register-medicine.component.html',
+  styleUrls: ['./register-medicine.component.css']
+})
+export class RegisterMedicineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
